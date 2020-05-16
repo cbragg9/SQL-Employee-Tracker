2c8d309e25d144b67087843618f1d44007d50619
Chris Bragg <cbragg9@yahoo.com>
2020-05-16 16:59:56 -0700

    inquirer and console table

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ A CLI employee management system using Node.js, Inquirer, and MySQL.
 
 
 ## Latest Commit  
--Connected to localhost MySQL  
--Added SQL schema and seeds  
+-Required npm console table for CLI formatting  
+-Added Inquirer prompts  
+-Added test query function and exit option    
 
 ## Previous Commits  
+-Connected to localhost MySQL  
+-Added SQL schema and seeds  
 -Added package.json  
 -Created template files  
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const pass = require("./private");
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -10,10 +11,44 @@ const connection = mysql.createConnection({
     database: "employee_tracker",
 });
 
+runPrompts();
 
-connection.query("SELECT * FROM role", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-    connection.end();
-});
+function runPrompts() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "userInput",
+            choices: [
+                "View All Employees", 
+                "View All Employees By Department", 
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "Exit"]
+        }
+    ])
+    .then(function (data) {
+        
+        switch (data.userInput) {
+            case "View All Employees":
+                queryAll();
+            break;
+            case "Exit":
+                connection.end();
+            break;
+        }
+    });
+}
+
+function queryAll() {
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        runPrompts();
+    });
+}
